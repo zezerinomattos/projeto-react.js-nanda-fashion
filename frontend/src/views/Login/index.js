85770dd3f1a56e153d9a1042fd92b8fdd0a8324b
zezerinodev <mattoszz@hotmail.com>
2022-08-11 22:53:28 -0300

    Login, acesso criado e conectado
    Foi testado e o login está conectando com o Firebase.

--- frontend/src/views/Login/index.js
+++ frontend/src/views/Login/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
+import 'firebase/auth';
+
+// MEUS ARQUIVOS
 import './styles.css';
+import firebase from '../../config/firebase';
 
 // IMAGENS
 import logoNanda from '../../assets/logo-Nanda.png';
 
 function Login() {
+
+    const [email, setEmail] = useState();
+    const [senha, setSenha] = useState();
+
+    function logar() {
+        firebase.auth().signInWithEmailAndPassword(email, senha)
+            .then(resultado => {
+                alert("Usuário Logado");
+            })
+            .catch(erro => {
+                alert(erro);
+            });
+    }
+
     return(
         <main className="login-content d-flex align-items-center ">
             <form className="form-signin mx-auto">
@@ -13,12 +31,13 @@ function Login() {
                     <h1 className="h3 mb-3 fw-normal ">Faça seu Login</h1>
                 </div>
 
-                <input type="email" className="form-control" id="floatingInput" placeholder="Email" />
+                <input type="email" className="form-control" id="floatingInput" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
                 
-                <input type="password" className="form-control my-2" id="floatingPassword" placeholder="Senha" />
+                <input type="password" className="form-control my-2" id="floatingPassword" placeholder="Senha" onChange={(e) => setSenha(e.target.value)} />
                 
 
-                <button className="btn-login w-100 btn btn-lg btn-primary my-1" type="button">Sign in</button>
+                <button onClick={logar} className="btn-login w-100 btn btn-lg btn-primary my-1" type="button">ENTRAR</button>
+
                 <div className="recuperar-senha mt-3 text-center">
                     <a href="#">Esqueceu a Senha?</a>
                 </div>
